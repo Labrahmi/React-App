b8c79b7e983cff20fbd0c39589a41988a00d201f
Youssef Labrahmi <yousseflabrahmi8@gmail.com>
2021-12-31 02:26:23 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -8,36 +8,7 @@ class App extends Component {
     render()
     {
         return (
-            <BrowserRouter>
-            <div className='prinUL'>
-                <ul>
-                    <Link to="/React-App">
-                        <li> Home </li>
-                    </Link>
-                    <Link to="/about">
-                        <li> About </li>
-                    </Link>
-                    <Link to="/contact">
-                        <li> Contact </li>
-                    </Link>
-                    <Link to="/project">
-                        <li> Project </li>
-                    </Link>
-                    <Link to="/skills">
-                        <li> Skills </li>
-                    </Link>
-
-                </ul>
-            </div>
-                <Routes>
-                        <Route exact path = '/' element = {<Home />} />
-                        <Route path = '/home' element = {<Home />} />
-                        <Route path = '/about' element = {<About />} />
-                        <Route path = '/contact' element = {<Contact />} />
-                        <Route path = '/project' element = {<Project />} />
-                        <Route path = '/skills' element = {<Skills />} />
-                </Routes>
-            </BrowserRouter>
+            <Home />
         )
     }
     
